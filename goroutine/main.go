c1a7b989908767a11d4cc8ad4657c6792bea09f4
kukku723 <z03177279@gmail.com>
2018-12-26 03:55:01 +0900

    [update] use_channel

--- goroutine/main.go
+++ goroutine/main.go
@@ -2,26 +2,59 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"runtime"
 	"time"
 )
 
-func f(ch chan int) {
-	msg := int{}
-	msg <- ch
-	msg = msg * 10
-	fmt.Printf("channel:%v\n", msg)
+func cf(ch chan int) {
+	msg := <-ch
+	fmt.Printf("channel内の値:%v\n", msg)
+	close(ch)
+
 }
+
+func s(num int) {
+	fmt.Printf("go print:%d\n", num)
+}
+
+func goroutine(ch chan string) {
+	for s := range ch {
+		log.Println("Hello", s)
+		time.Sleep(time.Second)
+	}
+}
+
 func main() {
-	go f(1)
-	go f(2)
-	go f(3)
+
+	go s(1)
+	//capacity　10
+	ch0 := make(chan int, 10)
+	ch0 <- 111
+	go cf(ch0)
+
 	//現在のgoroutine の数を表示
-	fmt.Println(runtime.NumGoroutine())
-	time.Sleep(time.Second)
+	fmt.Printf("現在のgoroutine数:%d\n", runtime.NumGoroutine())
+
 	go func() {
 		fmt.Println("終了")
 		runtime.Goexit()
 	}()
+	time.Sleep(time.Second)
 
+	ch1 := make(chan string, 5)
+
+	go goroutine(ch1)
+
+	ch1 <- "World0"
+	ch1 <- "World1"
+	ch1 <- "World2"
+	ch1 <- "World3"
+	ch1 <- "World4"
+	ch1 <- "World5"
+	ch1 <- "World6"
+	ch1 <- "World7"
+
+	fmt.Println("end")
+	time.Sleep(time.Second * 6)
 }
