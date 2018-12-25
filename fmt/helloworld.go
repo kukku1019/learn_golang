e9fd664fb393ed0720dd6d11d0d90356c1198f46
kukku723 <z03177279@gmail.com>
2018-12-26 01:48:45 +0900

    learn channel

--- fmt/helloworld.go
+++ fmt/helloworld.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+func test(n int) int {
+	var num int = 10
+	n = n * num
+	return n
+}
 
 func main() {
 	fmt.Println("Hello Wold,你好 世界！")
+	s := test(2)
+	fmt.Printf("funcの結果:%d", s)
+	time.Sleep(time.Second)
 }
--- goroutine/main.go
+++ goroutine/main.go
@@ -1,10 +1,27 @@
 package main
 
-import( 
+import (
 	"fmt"
+	"runtime"
+	"time"
 )
 
-func main {
-
+func f(ch chan int) {
+	msg := int{}
+	msg <- ch
+	msg = msg * 10
+	fmt.Printf("channel:%v\n", msg)
+}
+func main() {
+	go f(1)
+	go f(2)
+	go f(3)
+	//現在のgoroutine の数を表示
+	fmt.Println(runtime.NumGoroutine())
+	time.Sleep(time.Second)
+	go func() {
+		fmt.Println("終了")
+		runtime.Goexit()
+	}()
 
 }
